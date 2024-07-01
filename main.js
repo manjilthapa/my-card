960a69d3640337b8607f42c7babbe3646882c4da
manjilthapa <manjilthapa@gmail.com>
2024-07-01 10:08:43 +0200

    Task 2

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 "use strict";
-class MyCard extends HTMLElement {
+class NewElement extends HTMLElement {}
+class MyContainer extends HTMLElement {
+  constructor() {
+    super();
+    this.showText = false;
+  }
+  connectedCallback() {
+    this.addEventListener("toggle-text", this.onToggleText);
+    this.onToggleText = this.onToggleText.bind(this);
+  }
+  onToggleText() {
+    this.showText = !this.showText;
+    this.querySelectorAll("my-card").forEach((cardEl) => {
+      cardEl.toggleText(this.showText);
+    });
+  }
+}
+class MyCard extends NewElement {
   constructor() {
     super();
   }
@@ -11,6 +28,25 @@ class MyCard extends HTMLElement {
  <main>${this.getAttribute(
    "content"
  )}</main><span slot="my-text" class="my-text"> </slot></span><br/><slot class="btn" name="btn"></slot>`;
+    const btn = shadow.querySelector("slot.btn");
+    btn && btn.addEventListener("click", this.onClick);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick() {
+    this.dispatchEvent(
+      new CustomEvent("toggle-text", {
+        bubbles: true,
+        composed: true,
+      })
+    );
+  }
+  toggleText(value) {
+    var _a;
+    ((_a = this.shadowRoot) === null || _a === void 0
+      ? void 0
+      : _a.querySelector("span")
+    ).innerHTML = value ? "Button Clicked!" : "";
   }
 }
+customElements.define("my-container", MyContainer);
 customElements.define("my-card", MyCard);
--- main.ts
+++ main.ts
@@ -1,4 +1,31 @@
-class MyCard extends HTMLElement {
+abstract class NewElement extends HTMLElement {
+  abstract toggleText(value: boolean): void;
+}
+class MyContainer extends HTMLElement {
+  private showText: boolean;
+  constructor() {
+    super();
+
+    this.showText = false;
+  }
+
+  connectedCallback() {
+    this.addEventListener("toggle-text", this.onToggleText);
+    this.onToggleText = this.onToggleText.bind(this);
+  }
+
+  onToggleText() {
+    this.showText = !this.showText;
+
+    this.querySelectorAll<NewElement>("my-card").forEach(
+      (cardEl: NewElement) => {
+        cardEl.toggleText(this.showText);
+      }
+    );
+  }
+}
+
+class MyCard extends NewElement {
   constructor() {
     super();
   }
@@ -10,6 +37,26 @@ class MyCard extends HTMLElement {
  <main>${this.getAttribute(
    "content"
  )}</main><span slot="my-text" class="my-text"> </slot></span><br/><slot class="btn" name="btn"></slot>`;
+
+    const btn: HTMLButtonElement | null = shadow.querySelector("slot.btn");
+
+    btn && btn.addEventListener("click", this.onClick);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick() {
+    this.dispatchEvent(
+      new CustomEvent("toggle-text", {
+        bubbles: true,
+        composed: true,
+      })
+    );
+  }
+
+  toggleText(value: boolean) {
+    (this.shadowRoot?.querySelector("span") as HTMLSpanElement).innerHTML =
+      value ? "Button Clicked!" : "";
   }
 }
+
+customElements.define("my-container", MyContainer);
 customElements.define("my-card", MyCard);
